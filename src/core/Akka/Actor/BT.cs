5b86ea18c184e03f189446379e9aa4ab525b0349
dpavsrtrl <dpavlenkov@srtrl.com>
2017-03-30 17:20:33 -0400

    Created interfaces

---
+++ src/core/Akka/Actor/BT.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace Akka.Actor
+{
+    /// <summary>
+    /// Behavior Tree Actor
+    /// </summary>
+    /// <typeparam name="TData">Global data object accessible to all scopes.</typeparam>
+    public class BT<TData> : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public class TreeMachine
+        {
+            /// <summary>
+            /// Workflow Progress.
+            /// </summary>
+            public enum WorkflowStatus
+            {
+                Undetermined,
+                Running,
+                Success,
+                Failure
+            }
+
+            public interface IBlackboard : IDictionary<object, object>
+            {
+                IReadOnlyCollection<object> Messages { get; }
+            }
+
+            public interface IContext
+            {
+                TData GlobalData { get; }
+
+                IBlackboard ScopeData { get; }
+            }
+
+            public interface IWorkflow
+            {
+                WorkflowStatus Status { get; }
+
+                void Run(IContext context);
+            }
+
+            public interface IAction : IWorkflow
+            {
+            }
+
+            public interface IDecorator : IWorkflow
+            {
+                IWorkflow Next();
+            }
+
+            public interface ISplit : IWorkflow
+            {
+                IWorkflow[] Children { get; }
+            }
+
+            public interface ITransfer : IWorkflow
+            {
+                IWorkflow Child { get; }
+            }
+
+            public interface ISpawn : ITransfer
+            {
+            }
+
+            public interface IBecome : ITransfer
+            {
+            }
+
+            public interface IReceive : ITransfer
+            {
+            }
+        }
+    }
+}
