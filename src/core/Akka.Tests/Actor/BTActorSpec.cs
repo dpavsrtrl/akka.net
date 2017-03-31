6d79d03d071cd8362aabe0d60b5fc86785b4dbc5
dpavsrtrl <florida>
2017-03-31 17:56:17 -0400

    BTActorSpec

---
+++ src/core/Akka.Tests/Actor/BTActorSpec.cs
@@ -0,0 +1,259 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FSMActorSpec.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using Akka.Actor;
+using Akka.TestKit;
+using Akka.Util.Internal;
+using Xunit;
+
+namespace Akka.Tests.Actor
+{
+    public class BTActorSpec : AkkaSpec
+    {
+        #region Actors
+
+        public class ConstructorIncrement : BT<AtomicCounter>
+        {
+            public ConstructorIncrement(AtomicCounter counter, TestLatch latch)
+            {
+                counter.IncrementAndGet();
+                latch.CountDown();
+            }
+        }
+
+        public class ExecuteIncrement : BT<AtomicCounter>
+        {
+            public ExecuteIncrement(AtomicCounter counter, TestLatch latch)
+            {
+                StartWith(Execute(cxt =>
+                {
+                    cxt.GlobalData.IncrementAndGet();
+                }), counter);
+
+                latch.CountDown();
+            }
+        }
+
+        public class OneEcho : BT<object>
+        {
+            public OneEcho()
+            {
+                StartWith(ReceiveAny(
+                    Execute(ctx =>
+                    {
+                        Sender.Tell(ctx.CurrentMessage);
+                    })), null);
+            }
+        }
+
+        public class NestedEcho : BT<object>
+        {
+            public NestedEcho()
+            {
+                StartWith(
+                    ReceiveAny(
+                        ReceiveAny(
+                            Execute(ctx =>
+                            {
+                                Sender.Tell(ctx.CurrentMessage);
+                            }))), null);
+            }
+        }
+
+        public class SequenceOne : BT<List<string>>
+        {
+            public SequenceOne(List<string> pipe, TestLatch latch)
+            {
+                StartWith(Sequence(Execute(ctx => ctx.GlobalData.Add("1"))), pipe);
+
+                latch.CountDown();
+            }
+        }
+
+        public class SequenceTwo : BT<List<string>>
+        {
+            public SequenceTwo(List<string> pipe, TestLatch latch)
+            {
+                StartWith(Sequence(
+                    Execute(ctx => ctx.GlobalData.Add("1")),
+                    Execute(ctx => ctx.GlobalData.Add("2"))), pipe);
+
+                latch.CountDown();
+            }
+        }
+
+        public class SequenceReceive : BT<List<string>>
+        {
+            public SequenceReceive(List<string> pipe, TestLatch latch)
+            {
+                StartWith(Sequence(
+                    Execute(ctx => ctx.GlobalData.Add("1")),
+                    ReceiveAny(Execute(ctx => ctx.GlobalData.Add(ctx.CurrentMessage as string))),
+                    Execute(ctx => latch.CountDown())), pipe);
+            }
+        }
+
+        public class SequenceReceiveSequenceReceive : BT<List<string>>
+        {
+            public SequenceReceiveSequenceReceive(List<string> pipe, TestLatch latch)
+            {
+                StartWith(Sequence(
+                    ReceiveAny(Execute(ctx => ctx.GlobalData.Add(ctx.CurrentMessage as string))),
+                    Execute(ctx => ctx.GlobalData.Add("2")),
+                    Sequence(),
+                    Sequence(
+                        ReceiveAny(Sequence(
+                            Execute(ctx => ctx.GlobalData.Add(ctx.CurrentMessage as string)),
+                            ReceiveAny(Execute(ctx => ctx.GlobalData.Add(ctx.CurrentMessage as string)))
+                            ))
+                        ),
+                    Execute(ctx => latch.CountDown())), pipe);
+            }
+        }
+
+        public class LoopEcho : BT<object>
+        {
+            public LoopEcho()
+            {
+                StartWith(Loop(
+                    Sequence(
+                        ReceiveAny(Execute(ctx => Sender.Tell(ctx.CurrentMessage))),
+                        ReceiveAny(Execute(ctx => Sender.Tell(ctx.CurrentMessage))))), null);
+            }
+        }
+
+        #endregion
+
+        [Fact]
+        public void BTActor_ConstructorIncrement()
+        {
+            var counter = new AtomicCounter(0);
+            var latch = new TestLatch();
+
+            var bt = Sys.ActorOf(Props.Create(() => new ConstructorIncrement(counter, latch)));
+
+            latch.Ready();
+            Assert.Equal(1, counter.Current);
+        }
+
+        [Fact]
+        public void BTActor_ExecuteIncrement()
+        {
+            var counter = new AtomicCounter(0);
+            var latch = new TestLatch();
+
+            var bt = Sys.ActorOf(Props.Create(() => new ExecuteIncrement(counter, latch)));
+
+            latch.Ready();
+            Assert.Equal(1, counter.Current);
+        }
+
+        [Fact]
+        public void BTActor_OneEcho_Exactly_One()
+        {
+            var bt = Sys.ActorOf<OneEcho>();
+
+            bt.Tell("echo1", TestActor);
+
+            ExpectMsg((object)"echo1");
+
+            bt.Tell("echo1", TestActor);
+
+            ExpectNoMsg(100);
+        }
+
+        [Fact]
+        public void BTActor_NestedEcho_Second()
+        {
+            var bt = Sys.ActorOf<NestedEcho>();
+
+            bt.Tell("echo1", TestActor);
+            bt.Tell("echo2", TestActor);
+            ExpectMsg((object)"echo2");
+
+            bt.Tell("echo3", TestActor);
+
+            ExpectNoMsg(100);
+        }
+
+        [Fact]
+        public void BTActor_SequenceOne()
+        {
+            var pipe = new List<string>();
+            var latch = new TestLatch();
+
+            var bt = Sys.ActorOf(Props.Create(() => new SequenceOne(pipe, latch)));
+
+            latch.Ready();
+
+            Assert.Equal(new[] { "1" }, pipe);
+        }
+
+        [Fact]
+        public void BTActor_SequenceTwo()
+        {
+            var pipe = new List<string>();
+            var latch = new TestLatch();
+
+            var bt = Sys.ActorOf(Props.Create(() => new SequenceTwo(pipe, latch)));
+
+            latch.Ready();
+
+            Assert.Equal(new[] { "1", "2" }, pipe);
+        }
+
+        [Fact]
+        public void BTActor_SequenceReceive()
+        {
+            var pipe = new List<string>();
+            var latch = new TestLatch();
+
+            var bt = Sys.ActorOf(Props.Create(() => new SequenceReceive(pipe, latch)));
+
+            bt.Tell("2");
+
+            latch.Ready();
+
+            Assert.Equal(new[] { "1", "2" }, pipe);
+        }
+
+        [Fact]
+        public void BTActor_SequenceReceiveSequenceReceive()
+        {
+            var pipe = new List<string>();
+            var latch = new TestLatch();
+
+            var bt = Sys.ActorOf(Props.Create(() => new SequenceReceiveSequenceReceive(pipe, latch)));
+
+            bt.Tell("1");
+            bt.Tell("3");
+            bt.Tell("4");
+
+            latch.Ready();
+
+            Assert.Equal(new[] { "1", "2", "3", "4" }, pipe);
+        }
+
+        [Fact]
+        public void BTActor_Loop100()
+        {
+            var bt = Sys.ActorOf(Props.Create(() => new LoopEcho()));
+
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                bt.Tell(i.ToString(), TestActor);
+            }
+
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                ExpectMsg(i.ToString());
+            }
+        }
+    }
+}
--- src/core/Akka/Actor/BT.cs
+++ src/core/Akka/Actor/BT.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Akka.Actor
 {
@@ -9,17 +10,36 @@ namespace Akka.Actor
     /// <typeparam name="TData">Global data object accessible to all scopes.</typeparam>
     public class BT<TData> : UntypedActor
     {
+        protected TreeMachine Machine { get; set; }
+
         protected override void OnReceive(object message)
         {
-            throw new System.NotImplementedException();
+            if (!Machine.ProcessMessage(message))
+                Unhandled(message);
         }
 
-        public TreeMachine.ActionWF Execute(Action<TreeMachine.IContext> action)
+        protected TreeMachine.ActionWF Execute(Action<TreeMachine.IContext> action)
             => new TreeMachine.ActionWF(action);
 
+        protected TreeMachine.ReceiveAnyWF ReceiveAny(TreeMachine.IWorkflow child)
+            => new TreeMachine.ReceiveAnyWF(child);
+
+        protected TreeMachine.SequenceWF Sequence(params TreeMachine.IWorkflow[] children)
+            => new TreeMachine.SequenceWF(children);
+
+        protected TreeMachine.LoopWF Loop(TreeMachine.IWorkflow child)
+            => new TreeMachine.LoopWF(child);
+
+        protected void StartWith(TreeMachine.IWorkflow wf, TData data)
+        {
+            Machine = new TreeMachine(data, null);
+            Machine.Run(wf);
+        }
 
         public class TreeMachine
         {
+            private List<ScopeWF> _scopes = new List<ScopeWF>();
+
             private IBlackboard _rootBb;
 
             public TreeMachine(TData data, IBlackboard rootBb)
@@ -28,14 +48,179 @@ namespace Akka.Actor
                 _rootBb = rootBb;
             }
 
+            public bool ProcessMessage(object message)
+            {
+                return _scopes.Select(s => s.ProcessMessage(new WFContext(Data, message))).ToList().Any();
+            }
+
+            public void Run(IWorkflow wf)
+            {
+                _scopes.Add(new ScopeWF(wf));
+                Run();
+            }
+
+            public void Run()
+            {
+                _scopes.ForEach(s => s.Run(new WFContext(Data)));
+            }
+
+            protected class WFContext : IContext
+            {
+                public WFContext(TData data)
+                {
+                    GlobalData = data;
+                }
+                public WFContext(TData data, object message) : this(data)
+                {
+                    CurrentMessage = message;
+                }
+
+                public object CurrentMessage { get; }
+
+                public TData GlobalData { get; }
+
+                public IBlackboard ScopeData
+                {
+                    get
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+            }
             public TData Data { get; }
 
             public abstract class WFBase : IWorkflow
             {
-                public WorkflowStatus Status { get; protected set; }
+                public virtual WorkflowStatus Status { get; protected set; }
                 public object Result { get; protected set; }
 
                 public abstract void Run(IContext context);
+
+                public bool IsComplete => Status == WorkflowStatus.Success || Status == WorkflowStatus.Failure;
+
+                public abstract void Reset();
+            }
+
+            public class ScopeWF : WFBase, ITransfer
+            {
+                private IWorkflow _child;
+                private Stack<IWorkflow> _stack = new Stack<IWorkflow>();
+
+                public ScopeWF(IWorkflow child)
+                {
+                    _child = child;
+                    Status = _child.Status;
+                }
+                public IWorkflow Child => _child;
+
+                public override void Reset()
+                {
+
+                }
+
+                public override void Run(IContext context)
+                {
+                    bool done = false;
+                    do
+                    {
+                        Status = _child.Status;
+
+                        if (IsComplete)
+                        {
+                            if (_stack.Count > 0)
+                            {
+                                _child = _stack.Pop();
+                            }
+                            else
+                            {
+                                done = true;
+                            }
+                        }
+                        else
+                        {
+                            if (!(_child is IReceive))
+                            {
+                                _child.Run(context);
+
+                                var decorator = _child as IDecorator;
+                                if (decorator != null)
+                                {
+                                    if (decorator.Status == WorkflowStatus.Running)
+                                    {
+                                        _stack.Push(decorator);
+
+                                        _child = decorator.Next();
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                done = true;
+                            }
+                        }
+                    } while (!done);
+                }
+
+                public bool ProcessMessage(IContext context)
+                {
+                    if (IsComplete)
+                        return false;
+
+                    var receiver = _child as IReceive;
+
+                    if (receiver != null)
+                    {
+                        if (receiver.ProcessMessage(context.CurrentMessage))
+                        {
+                            if (!IsComplete)
+                            {
+                                _stack.Push(_child);
+                                _child = receiver.Child;
+
+                                Run(context);
+                            }
+
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+            }
+
+            public class ReceiveAnyWF : WFBase, IReceive<object>
+            {
+                private bool _hasProcessed;
+
+                public ReceiveAnyWF(IWorkflow child)
+                {
+                    Child = child;
+                }
+
+                public override WorkflowStatus Status
+                    => _hasProcessed ? Child?.Status ?? WorkflowStatus.Success : WorkflowStatus.Undetermined;
+
+                public IWorkflow Child { get; }
+
+                public override void Reset()
+                {
+                    Child?.Reset();
+                    _hasProcessed = false;
+                }
+
+                public override void Run(IContext context)
+                {
+                    throw new InvalidOperationException("Receive tasks don't run.");
+                }
+
+                public virtual bool ProcessMessage(object message)
+                {
+                    Status = Child?.Status ?? WorkflowStatus.Success;
+
+                    _hasProcessed = true;
+
+                    return _hasProcessed;
+                }
             }
 
             public class ActionWF : WFBase
@@ -45,6 +230,11 @@ namespace Akka.Actor
                 public ActionWF(Action<IContext> action)
                 {
                     _action = action;
+                }
+
+                public override void Reset()
+                {
+                    Status = WorkflowStatus.Undetermined;
                 }
 
                 public override void Run(IContext context)
@@ -65,6 +255,96 @@ namespace Akka.Actor
                 }
             }
 
+            public class SequenceWF : WFBase, IDecorator
+            {
+                private List<IWorkflow> _children;
+                private IWorkflow _current;
+                private int _pos = 0;
+
+                public SequenceWF(params IWorkflow[] children)
+                {
+                    _children = new List<IWorkflow>(children);
+                }
+
+                public override void Reset()
+                {
+                    _pos = 0;
+                    _children.ForEach(c => c.Reset());
+                    Status = WorkflowStatus.Undetermined;
+                }
+
+                public IWorkflow Next()
+                {
+                    if (_pos < _children.Count)
+                    {
+                        _current = _children[_pos];
+                        _pos++;
+                    }
+                    else
+                    {
+                        _current = null;
+                    }
+
+                    return _current;
+                }
+
+                public override void Run(IContext context)
+                {
+                    if (_current?.Status == WorkflowStatus.Failure)
+                    {
+                        Status = WorkflowStatus.Failure;
+
+                        return;
+                    }
+
+                    if (!IsComplete)
+                    {
+                        if (_pos >= _children.Count)
+                        {
+                            Status = WorkflowStatus.Success;
+                        }
+                        else
+                        {
+                            Status = WorkflowStatus.Running;
+                        }
+                    }
+                }
+            }
+
+            public class LoopWF : WFBase, IDecorator
+            {
+                private IWorkflow _child;
+
+                public LoopWF(IWorkflow child)
+                {
+                    _child = child;
+                }
+
+                public override void Reset()
+                {
+                    Status = WorkflowStatus.Undetermined;
+                    _child?.Reset();
+                }
+
+                public IWorkflow Next()
+                {
+                    _child?.Reset();
+                    return _child;
+                }
+
+                public override void Run(IContext context)
+                {
+                    if (_child?.Status == WorkflowStatus.Failure)
+                    {
+                        Status = WorkflowStatus.Failure;
+                    }
+                    else
+                    {
+                        Status = WorkflowStatus.Running;
+                    }
+                }
+            }
+
             /// <summary>
             /// Workflow Progress.
             /// </summary>
@@ -76,20 +356,48 @@ namespace Akka.Actor
                 Failure
             }
 
+            /// <summary>
+            /// Blackboard allows extension. Extension emulates local scope.
+            /// Extended blackboard can hide dictionary entries of previous scope blackboard.
+            /// Lower scope blackboard cannot modify higher scope blackboard.
+            /// </summary>
             public interface IBlackboard : IDictionary<object, object>
             {
-                IReadOnlyCollection<object> Messages { get; }
+                /// <summary>
+                /// Concatenation of blackboard messages from all scopes.
+                /// </summary>
+                IReadOnlyList<object> Messages { get; }
 
+                /// <summary>
+                /// Prepend message.
+                /// </summary>
+                /// <param name="msg"></param>
                 void PushMessage(object msg);
+
+                /// <summary>
+                /// Remove first message. Only local blackboard is affected.
+                /// When local message list is empty, return null.
+                /// </summary>
+                /// <returns>Message object or null.</returns>
                 object PopMessage();
 
-                IBlackboard Insert();
-                void Retract(IBlackboard bb);
+                /// <summary>
+                /// Extend blackboard for local modification.
+                /// </summary>
+                /// <returns>Extended blackboard.</returns>
+                IBlackboard Extend();
+                /// <summary>
+                /// Discard extended blackboard.
+                /// </summary>
+                /// <returns>Previous blackboard.</returns>
+                IBlackboard Retract();
             }
 
             public interface IContext
             {
                 TData GlobalData { get; }
+
+                object CurrentMessage { get; }
 
                 IBlackboard ScopeData { get; }
             }
@@ -98,7 +406,11 @@ namespace Akka.Actor
             {
                 WorkflowStatus Status { get; }
 
+                object Result { get; }
+
                 void Run(IContext context);
+
+                void Reset();
             }
 
             public interface IAction : IWorkflow
@@ -130,6 +442,12 @@ namespace Akka.Actor
 
             public interface IReceive : ITransfer
             {
+                bool ProcessMessage(object message);
+            }
+
+            public interface IReceive<T> : IReceive
+            {
+                bool ProcessMessage(T message);
             }
         }
     }
